cab8121297c1228332a3380ddf2d1426f24f0a23
Aly-Tamboura <alytamboura7370@gmail.com>
2017-08-07 08:33:02 -0700

    ajax to api

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
 import API from '../../config/index.js';
+import css from './style.scss';
 
 class App extends React.Component {
 	constructor(props) {
