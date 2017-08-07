3d1c4481300bf74f1f4ec970ad577f9ed7c44c2b
Aly-Tamboura <alytamboura7370@gmail.com>
2017-08-07 08:39:57 -0700

    ajax and div style

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # BrightWeather
+
+## Guide
+
+# Application Construction
+- Front end: React.js
+- Back end: node.js and express
+- Compiler: web-pack
+- Styles: Sass (minified by web-pack)
+
+# Build
+- Fork repo.
+- Go to the config directory and add the api key to the example.index.js file and rename the file to index.js.
+- Run npm install to download dependencies.
+- In the terminal 'npm start' to start node.
+- In the terminal 'npm build' to start web-pack.
+- Open the browser to 'localhost:8080' to run app in the browser.
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -7,11 +7,31 @@ import css from './style.scss';
 class App extends React.Component {
 	constructor(props) {
 		super(props)
+		this.getWeatherData = this.getWeatherData.bind(this);
   }
+  componentDidMount() {
+		this.getWeatherData('san jose')
+  }
+
+  getWeatherData(city) {
+		$.ajax({
+			url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${API.weatherunderground}`,
+			type: 'GET',
+      data: 'application/json',
+			success: function(data) {
+        console.log(data)
+			}.bind(this),
+			error: function(err) {
+				if(err) {
+					console.log('not found');
+				}
+			}.bind(this)
+		})
+	}
 
 	render() {
 		return (
-      <div>
+      <div className='container'>
       	<h1>What's the Weather in Your City?</h1>
       </div>
 		)
